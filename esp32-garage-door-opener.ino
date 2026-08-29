771e8657c1187965b74b2f747176b1c323b2c00f
Muhammad Fikri <muhammadfikripersonalmail@gmail.com>
2026-08-29 06:19:15 +0000

    Initial commit

---
+++ esp32-garage-door-opener.ino
@@ -0,0 +1,21 @@
+#define PIN_RELAY_TRIGGER 26
+#define PIN_REED_CLOSED 27
+
+void toggleDoor() {
+ digitalWrite(PIN_RELAY_TRIGGER, LOW);
+ delay(500);
+ digitalWrite(PIN_RELAY_TRIGGER, HIGH);
+}
+
+void setup() {
+ Serial.begin(115200);
+ pinMode(PIN_RELAY_TRIGGER, OUTPUT);
+ digitalWrite(PIN_RELAY_TRIGGER, HIGH);
+ pinMode(PIN_REED_CLOSED, INPUT_PULLUP);
+}
+
+void loop() {
+ bool isClosed = (digitalRead(PIN_REED_CLOSED) == LOW);
+ Serial.printf("Garage Door Status: %s\n", isClosed ? "CLOSED" : "OPEN");
+ delay(2000);
+}
